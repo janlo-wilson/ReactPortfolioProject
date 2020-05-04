ac69960cf8dfd63e763366558f877503d211f3a0
Jan Wilson <janlo.wilson@gmail.com>
2020-05-04 16:11:54 -0700

    initial commit

---
+++ src/Components/CalendarComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+export default Calendar;
---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen: false
+        };
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <h1>What's Up North County!</h1>
+                                <h2>Your One-Stop Shop for Events</h2>
+                            </div>
+                        </div>
+                    </div>
+                </Jumbotron>
+                <Navbar dark sticky="top" expand="md">
+                    <div className="container">
+                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png"
+                            height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <i className="fa fa-home fa-lg" /> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/events">
+                                        <i className="fa fa-bullhorn fa-lg" /> Events
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/calendar">
+                                        <i className="fa fa-calendar fa-lg" /> Calendar
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <i className="fa fa-address-card fa-lg" /> Contact
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
